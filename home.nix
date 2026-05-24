89ae8bb7b74b0c3a422992d984876bc397a3da0c
Mark Rossi <mark.rossi.06@gmail.com>
2026-05-24 16:04:43 -0400

    hopefully fix zed

--- home.nix
+++ home.nix
@@ -7,12 +7,25 @@
 
   programs.starship.enable = true;
 
+  programs.zed-editor = {
+    enable = true;
+    extensions = [ "nix" "toml" "rust" ];
+    userSettings = {
+      theme = {
+        mode = "system";
+        dark = "One Dark";
+        light = "One Light";
+      };
+    vim_mode = true;
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    initExtra= ''
+    profileExtra = ''
       eval "$(starship init zsh)"
 
       if uwsm check may-start && uwsm select; then
@@ -22,5 +35,12 @@
   };
 
   # home.file.".config/hypr".source = ./config/hypr;
-  # home.file.".config/zed".source = ./config/zed;
+  home.file.".config/ghostty".source = ./config/ghostty;
+  home.file.".config/starship".source = ./config/starship;
+
+  home.packages = with pkgs; [
+    commit-mono
+  ];
+
+  fonts.fontconfig.enable = true;
 }
