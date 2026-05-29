9997b3c591fc066d744518ffb8fce5ccbcb5d6c6
Mark Rossi <mark.rossi.06@gmail.com>
2026-05-29 10:36:52 -0400

    add paneru config for work-mac host

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    paneru = {
+      url = "github:karinushka/paneru";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
--- home/hosts/work-mac.nix
+++ home/hosts/work-mac.nix
@@ -1,10 +1,61 @@
-{ ... }:
+{ inputs, ... }:
 {
   imports = [
     ../default.nix
     ../modules/common.nix
     ../modules/darwin.nix
+
+    inputs.paneru.homeModules.paneru
   ];
 
-  # Host-specific Home Manager settings for the work Mac go here.
+  services.paneru = {
+    enable = true;
+    settings = {
+      options = {
+        focus_follows_mouse = false;
+        mouse_follows_focus = true;
+        preset_column_widths = [
+          0.25
+          0.33
+          0.5
+          0.66
+          0.75
+          0.8
+        ];
+        border_active_window = true;
+        border_color = "#679D6B";
+        border_width = 1.0;
+
+        swipe_gesture_fingers = 4;
+
+        animation_speed = 50;
+      };
+
+      bindings = {
+        quit = "ctrl + alt - q";
+
+        window_focus_west = "cmd - h";
+        window_focus_east = "cmd - l";
+        window_focus_north = "cmd - k";
+        window_focus_south = "cmd - j";
+
+        window_swap_west = "cmd + alt - h";
+        window_swap_east = "cmd + alt - l";
+        window_swap_north = "cmd + alt - k";
+        window_swap_south = "cmd + alt - j";
+
+        window_center = "alt - c";
+        window_resize = "alt - r";
+        window_shrink = "alt + shift - r";
+
+        window_fullwidth = "alt - f";
+        window_manage = "alt - v";
+
+        window_stack = "alt - ]";
+        window_unstack = "alt + shift - ]";
+
+        window_equalize = "alt + shift - e";
+      };
+    };
+  };
 }
